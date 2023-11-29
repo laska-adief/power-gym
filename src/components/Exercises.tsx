f4c398bfe1aa7d9595ea4147c4555639351de279
laska-adief <laskaadief.la@gmail.com>
2023-11-29 14:44:25 +0700

    [Power Gym] add ui loading skeleton while fetching data

--- src/components/Exercises.tsx
+++ src/components/Exercises.tsx
@@ -2,18 +2,48 @@ import Grid from "@mui/material/Grid";
 import { useExerciseStore } from "../hooks/useExerrcise";
 import { ExerciseProp } from "../types/exercise";
 import CardExercise from "./CardExercise";
+import { Skeleton, Stack } from "@mui/material";
 
 const Exercises = () => {
+  const loadingExercise = useExerciseStore((state) => state.loadingExercise);
   const exercisesData = useExerciseStore((state) => state.exercises);
-  return (
+  const searchValue = useExerciseStore((state) => state.searchValue);
+  const searchedExercises = exercisesData.filter(
+    (exercise: ExerciseProp) =>
+      exercise.name.includes(searchValue) ||
+      exercise.bodyPart.includes(searchValue) ||
+      exercise.equipment.includes(searchValue) ||
+      exercise.target.includes(searchValue)
+  );
+
+  const LoadingDisplay = () => (
+    <Stack justifyContent={"center"} alignContent={"center"}>
+      <Grid container spacing={4}>
+        {Array.from({ length: 12 }, (_, index) => (
+          <Grid item key={index} xs={12} sm={6} md={4} xl={3} minWidth={264}>
+            <Skeleton variant="rounded" sx={{ width: "100%", height: { xs: 358, sm: 260, md: 262, lg: 362, xl: 264 }, mb: 1 }}></Skeleton>
+            <Stack direction={"row"} spacing={1} mb={1}>
+              <Skeleton variant="rounded" height={32} width={62} sx={{ borderRadius: 4 }}></Skeleton>
+              <Skeleton variant="rounded" height={32} width={62} sx={{ borderRadius: 4 }}></Skeleton>
+            </Stack>
+            <Skeleton variant="rounded" height={32} sx={{ width: "80%" }}></Skeleton>
+          </Grid>
+        ))}
+      </Grid>
+    </Stack>
+  );
+
+  const DataDisplay = () => (
     <Grid container spacing={4}>
-      {exercisesData?.length &&
-        exercisesData?.map((exercise: ExerciseProp) => (
-          <Grid item key={exercise?.id} xs={12} md={4} xl={3}>
+      {searchedExercises?.length &&
+        searchedExercises?.map((exercise: ExerciseProp) => (
+          <Grid item key={exercise?.id} xs={12} sm={6} md={4} xl={3}>
             <CardExercise exercise={exercise} />
           </Grid>
         ))}
     </Grid>
   );
+
+  return loadingExercise ? <LoadingDisplay /> : <DataDisplay />;
 };
 export default Exercises;
