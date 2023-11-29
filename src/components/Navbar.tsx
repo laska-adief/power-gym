fb17e6b0c811742af83a1a1e66b051101dd53b1e
laska-adief <laskaadief.la@gmail.com>
2023-11-29 10:18:42 +0700

    [Power Gym] navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import { Stack } from "@mui/material";
+import Logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <Stack direction="row" alignItems="center" gap={10} py={4}>
+      <Link to="/">
+        <img src={Logo} alt="logo" width={48} height={48} />
+      </Link>
+
+      <Stack direction="row" gap={2}>
+        <Link to="/" style={{ fontSize: "24px", fontWeight: "bold", color: "#22092C" }}>
+          Home
+        </Link>
+        <Link to="/exercise" style={{ fontSize: "24px", fontWeight: "bold", color: "#22092C" }}>
+          Exercise
+        </Link>
+      </Stack>
+    </Stack>
+  );
+};
+export default Navbar;
