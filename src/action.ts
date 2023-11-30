f61f1160107a8d168fa0ed9f193e242a8e6bb330
laska-adief <laskaadief.la@gmail.com>
2023-11-30 19:43:28 +0700

    [Power Gym] add action query to get exercise by category

--- src/action.ts
+++ src/action.ts
@@ -7,6 +7,12 @@ export const fetchDataExercises = async () => {
   return data;
 };
 
+export const fetchDataExercisesByCategory = async (category: string) => {
+  const url = `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${category}?limit=1324`;
+  const data: ExerciseProp[] = await fetchData(url, options);
+  return data;
+};
+
 export const fetchDataBodyPartList = async () => {
   const url = "https://exercisedb.p.rapidapi.com/exercises/bodyPartList";
   const data: string[] = await fetchData(url, options);
