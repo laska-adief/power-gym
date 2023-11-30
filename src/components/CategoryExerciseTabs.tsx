4c1152a407af45fadd32358b278ec2dc14f1f3f2
laska-adief <laskaadief.la@gmail.com>
2023-11-30 21:41:14 +0700

    [Power Gym] add loading state and view

--- src/components/CategoryExerciseTabs.tsx
+++ src/components/CategoryExerciseTabs.tsx
@@ -1,6 +1,6 @@
-import { Box, Tab, Tabs } from "@mui/material";
+import { Box, Skeleton, Stack, Tab, Tabs } from "@mui/material";
 import { useExerciseStore } from "../hooks/useExerrcise";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchDataBodyPartList, fetchDataExercisesByCategory } from "../action";
 
 const CategoryExerciseTabs = () => {
@@ -10,14 +10,20 @@ const CategoryExerciseTabs = () => {
   const setbodyPartList = useExerciseStore((state) => state.setBodyPartList);
   const setExercise = useExerciseStore((state) => state.setExercise);
   const setLoadingExercise = useExerciseStore((state) => state.setLoadingExercise);
+  const [loadingCategory, setLoadingCategory] = useState(false);
 
   useEffect(() => {
+    setLoadingCategory(true);
     const fetchData = async () => {
       try {
         const bodyPartListData = await fetchDataBodyPartList();
-        setbodyPartList(bodyPartListData);
+        if (bodyPartListData?.length) {
+          setbodyPartList(bodyPartListData);
+          setLoadingCategory(false);
+        }
       } catch (error) {
         console.error("Error fetching exercises:", error);
+        setLoadingCategory(false);
       }
     };
 
@@ -43,66 +49,75 @@ const CategoryExerciseTabs = () => {
     setCategory(newValue);
     getDataExerciseByCategory(newValue);
   };
-  return (
-    <Box mb={4}>
-      <Tabs
-        value={category}
-        onChange={handleChangeCategory}
-        aria-label="Category Body Parts"
-        variant="scrollable"
-        sx={{
-          "& .MuiTabs-flexContainer": {
-            display: "flex",
-            justifyContent: "start",
-            alignItems: "center",
-            gap: 1,
-          },
-          "& .MuiButtonBase-root.Mui-selected": {
-            background: "#BE3144 !important",
-            color: "#FFF !important",
-          },
-          "& .MuiTabs-indicator": {
-            background: "transparent !important",
-          },
-        }}>
-        <Tab
-          label={"All"}
-          value={"all"}
-          sx={{
-            background: "#FFF",
-            border: "2px solid #BE3144",
-            color: "#BE3144",
-            borderRadius: 4,
-            fontWeight: 500,
-            transition: "all .3 ease",
-            "&:hover": {
-              background: "#BE3144",
-              color: "#FFF",
-            },
-          }}
-        />
-        {bodyPartList?.length &&
-          bodyPartList.map((item: string, index: number) => (
-            <Tab
-              key={index}
-              label={item}
-              value={item}
-              sx={{
-                background: "#FFF",
-                border: "2px solid #BE3144",
-                color: "#BE3144",
-                borderRadius: 4,
-                fontWeight: 500,
-                transition: "all .3 ease",
-                "&:hover": {
-                  background: "#BE3144",
-                  color: "#FFF",
-                },
-              }}
-            />
-          ))}
-      </Tabs>
-    </Box>
+
+  const LoadingCategory = () => (
+    <Stack direction={"row"} gap={1}>
+      {Array.from({ length: 12 }, (_, index) => (
+        <Skeleton variant="rounded" width={90} height={48} key={index}></Skeleton>
+      ))}
+    </Stack>
   );
+
+  const DisplayCategory = () => (
+    <Tabs
+      value={category}
+      onChange={handleChangeCategory}
+      aria-label="Category Body Parts"
+      variant="scrollable"
+      sx={{
+        "& .MuiTabs-flexContainer": {
+          display: "flex",
+          justifyContent: "start",
+          alignItems: "center",
+          gap: 1,
+        },
+        "& .MuiButtonBase-root.Mui-selected": {
+          background: "#BE3144 !important",
+          color: "#FFF !important",
+        },
+        "& .MuiTabs-indicator": {
+          background: "transparent !important",
+        },
+      }}>
+      <Tab
+        label={"All"}
+        value={"all"}
+        sx={{
+          background: "#FFF",
+          border: "2px solid #BE3144",
+          color: "#BE3144",
+          borderRadius: 4,
+          fontWeight: 500,
+          transition: "all .3 ease",
+          "&:hover": {
+            background: "#BE3144",
+            color: "#FFF",
+          },
+        }}
+      />
+      {bodyPartList?.length &&
+        bodyPartList.map((item: string, index: number) => (
+          <Tab
+            key={index}
+            label={item}
+            value={item}
+            sx={{
+              background: "#FFF",
+              border: "2px solid #BE3144",
+              color: "#BE3144",
+              borderRadius: 4,
+              fontWeight: 500,
+              transition: "all .3 ease",
+              "&:hover": {
+                background: "#BE3144",
+                color: "#FFF",
+              },
+            }}
+          />
+        ))}
+    </Tabs>
+  );
+
+  return <Box mb={4}>{loadingCategory ? <LoadingCategory /> : <DisplayCategory />}</Box>;
 };
 export default CategoryExerciseTabs;
