b174188dc83de6fba0c2413c386e6a09cf09640b
laska-adief <laskaadief.la@gmail.com>
2023-12-01 22:40:02 +0700

    [Power Gym] add fetch data detail exercise

--- src/action.ts
+++ src/action.ts
@@ -7,6 +7,12 @@ export const fetchDataExercises = async () => {
   return data;
 };
 
+export const fetchDataExercisesById = async (id: string) => {
+  const url = `https://exercisedb.p.rapidapi.com/exercises/exercise/${id}`;
+  const data: ExerciseProp = await fetchData(url, options);
+  return data;
+};
+
 export const fetchDataExercisesByCategory = async (category: string) => {
   const url = `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${category}?limit=1324`;
   const data: ExerciseProp[] = await fetchData(url, options);
