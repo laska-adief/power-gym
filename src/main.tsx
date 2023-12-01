9c659e409631f2b52d749723df4333d7023db017
laska-adief <laskaadief.la@gmail.com>
2023-12-02 00:52:16 +0700

    [Power Gym] add conditoinal basename in BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+const basename = process.env.NODE_ENV === "production" ? "/power-gym" : "/";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
