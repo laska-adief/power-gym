4dfc3dd56904229f276b8fe66f0e0476b6ec90c8
laska-adief <laskaadief.la@gmail.com>
2023-12-02 01:18:01 +0700

    [Power Gym] add env and configure base name

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,8 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-const basename = process.env.NODE_ENV === "production" ? "/power-gym" : "/";
+const isProduction = process.env.NODE_ENV === "production";
+const basename = isProduction ? process.env.VITE_REACT_APP_BASE_URL_PRODUCTION : process.env.VITE_REACT_APP_BASE_URL_DEVELOPMENT;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const isProduction = process.env.NODE_ENV === "production";
+const basename = isProduction ? process.env.VITE_REACT_APP_BASE_URL_PRODUCTION : process.env.VITE_REACT_APP_BASE_URL_DEVELOPMENT;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/power-gym/" : "/",
+  base: basename,
 });
