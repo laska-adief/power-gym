f3e4dff64919dbea1c8eb37aeede766f6397b00f
laska-adief <laskaadief.la@gmail.com>
2023-12-02 02:08:42 +0700

    [Power Gym] fix redirect link home

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,12 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <Stack direction="row" alignItems="center" gap={10} py={4}>
-      <Link to="/">
+      <Link to="/power-gym">
         <img src={Logo} alt="logo" width={48} height={48} />
       </Link>
 
       <Stack direction="row" gap={2}>
-        <Link to="/" style={{ fontSize: "24px", fontWeight: "bold", color: "#22092C" }}>
+        <Link to="/power-gym" style={{ fontSize: "24px", fontWeight: "bold", color: "#22092C" }}>
           Home
         </Link>
       </Stack>
