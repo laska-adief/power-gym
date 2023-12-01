652658be187902bde4699a4bdc23ae594b12b7cc
laska-adief <laskaadief.la@gmail.com>
2023-12-02 01:24:43 +0700

    [Pwer Gym] add import.meta.env to import env

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-const isProduction = process.env.NODE_ENV === "production";
-const basename = isProduction ? process.env.VITE_REACT_APP_BASE_URL_PRODUCTION : process.env.VITE_REACT_APP_BASE_URL_DEVELOPMENT;
+const isProduction = import.meta.env.NODE_ENV === "production";
+const basename = isProduction ? import.meta.env.VITE_REACT_APP_BASE_URL_PRODUCTION : import.meta.env.VITE_REACT_APP_BASE_URL_DEVELOPMENT;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
